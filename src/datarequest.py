59c0b6525b403c8f2a20804d92992fce205e73de
Tyler Shoemake <tyshoe2010@gmail.com>
2023-02-25 11:18:15 -0600

    added run count by game logic

--- src/datarequest.py
+++ src/datarequest.py
@@ -1,50 +1,56 @@
 import datetime
 import requests
 
-HEADERS = {'User-Agent': 'SpeedrunTopThree/1.0'}
+HEADERS = {"User-Agent": "SpeedrunTopThree/1.0"}
 
 
 def getDataFromJson(url):
     """Gets data from dictionary of json file from HTTP request"""
-    r = (requests.get(url, headers=HEADERS))
-    print(r)
-    return r.json()['data']
+    r = requests.get(url, headers=HEADERS)
+    # print(r)
+    return r.json()["data"]
 
 
 def getUserProfile(profileToSearch):
-    """Gets data about user with parameter of user """
-    url = "https://www.speedrun.com/api/v1/users?lookup={}".format(
-        profileToSearch)
+    """Gets data about user with parameter of user"""
+
+    url = "https://www.speedrun.com/api/v1/users?lookup={}".format(profileToSearch)
 
     data = getDataFromJson(url)[0]
-
-    # print()
-    print('DATA: ' + str(data))
+    # print("DATA: " + str(data))
 
     # Set variables from data
-    userId = data['id']
-    userName = data['names']['international']
-    speedrunLink = data['weblink']
-    twitchLink = data['twitch'] if data['twitch'] is None else data['twitch']['uri']
-    youtubeLink = data['youtube'] if data['youtube'] is None else data['youtube']['uri']
-    twitterLink = data['twitter'] if data['twitter'] is None else data['twitter']['uri']
-    speedrunsLiveLink = data['speedrunslive'] if data['speedrunslive'] is None else data['speedrunslive']['uri']
+    userId = data["id"]
+    userName = data["names"]["international"]
+    speedrunLink = data["weblink"]
+    twitchLink = data["twitch"] if data["twitch"] is None else data["twitch"]["uri"]
+    youtubeLink = data["youtube"] if data["youtube"] is None else data["youtube"]["uri"]
+    twitterLink = data["twitter"] if data["twitter"] is None else data["twitter"]["uri"]
+    speedrunsLiveLink = (
+        data["speedrunslive"]
+        if data["speedrunslive"] is None
+        else data["speedrunslive"]["uri"]
+    )
     # Grabs data from date timezone string
-    signup = (datetime.datetime.strptime(data['signup'], "%Y-%m-%dT%H:%M:%SZ"))
+    signup = datetime.datetime.strptime(data["signup"], "%Y-%m-%dT%H:%M:%SZ")
     userSignup = signup.strftime("%m-%d-%Y")
 
-    userProfile = {'userId': userId, 'speedrunLink': speedrunLink,
-                   'twitchLink': twitchLink, 'youtubeLink': youtubeLink,
-                   'twitterLink': twitterLink, 'speedrunsLiveLink': speedrunsLiveLink,
-                   'userName': userName, 'userSignup': userSignup}
-
+    userProfile = {
+        "userId": userId,
+        "speedrunLink": speedrunLink,
+        "twitchLink": twitchLink,
+        "youtubeLink": youtubeLink,
+        "twitterLink": twitterLink,
+        "speedrunsLiveLink": speedrunsLiveLink,
+        "userName": userName,
+        "userSignup": userSignup,
+    }
     return userProfile
 
 
 def getPersonalBests(userId):
-    """Gets personal bests data from userId """
-    url = "https://www.speedrun.com/api/v1/users/{}/personal-bests".format(
-        userId)
+    """Gets personal bests data from userId"""
+    url = "https://www.speedrun.com/api/v1/users/{}/personal-bests".format(userId)
     data = getDataFromJson(url)
 
     # check if user has runs
@@ -56,61 +62,105 @@ def getPersonalBests(userId):
     thirdPlace = 0
     personalBests = len(data)
 
+    startTime = datetime.datetime.utcnow().strftime("%H:%M:%S.%f")
     # print ('DATA Len: {}'.format(len(data)))
     for run in range(len(data)):
-        if data[run]['place'] == 1:
+        if data[run]["place"] == 1:
             firstPlace += 1
-        elif data[run]['place'] == 2:
+        elif data[run]["place"] == 2:
             secondPlace += 1
-        elif data[run]['place'] == 3:
+        elif data[run]["place"] == 3:
             thirdPlace += 1
-        # print ('DATA {}: {}'.format(run, (data[0])))
-        # print ('')
+        print("DATA {}: {}".format(run, (data[0])))
+        print("")
 
-    print('1st Place runs: {}'.format(str(firstPlace)))
-    print('2nd Place runs: {}'.format(str(secondPlace)))
-    print('3rd Place runs: {}'.format(str(thirdPlace)))
+    # print("{}".format(startTime))
+    # print("1st Place runs: {}".format(str(firstPlace)))
+    # print("2nd Place runs: {}".format(str(secondPlace)))
+    # print("3rd Place runs: {}".format(str(thirdPlace)))
 
-    personalBestsData = {'personalBests': personalBests, 'firstPlace': firstPlace,
-                         'secondPlace': secondPlace, 'thirdPlace': thirdPlace}
+    personalBestsData = {
+        "personalBests": personalBests,
+        "firstPlace": firstPlace,
+        "secondPlace": secondPlace,
+        "thirdPlace": thirdPlace,
+    }
 
     return personalBestsData
 
 
 def getRunCount(userId):
-    """Gets run data from userId """
-    url = "https://www.speedrun.com/api/v1/runs?user={}&max=200".format(
-        userId)
+    """Gets run data from userId"""
+    url = "https://www.speedrun.com/api/v1/runs?user={}&max=200".format(userId)
     data = getDataFromJson(url)
 
     if not data:
         return {}
 
+    # print("DATA {}".format((data)))
     verifiedRuns = 0
     rejectedRuns = 0
+    runCountByGame = {}
 
     for run in range(len(data)):
-        # print(data[run]['status']['status'])
-        if data[run]['status']['status'] == 'verified':
+        # print(data[run])
+        if data[run]["status"]["status"] == "verified":
             verifiedRuns += 1
-        elif data[run]['status']['status'] == 'rejected':
+        elif data[run]["status"]["status"] == "rejected":
             rejectedRuns += 1
+        # print(data[run]["game"])
 
-    print('Total Runs: {}'.format(len(data)))
-    print('Verified Runs: {}'.format(verifiedRuns))
-    print('Rejected Runs: {}'.format(rejectedRuns))
+    # print("Total Runs: {}".format(len(data)))
+    # print("Verified Runs: {}".format(verifiedRuns))
+    # print("Rejected Runs: {}".format(rejectedRuns))
 
-    runCountData = {'totalRuns': len(data), 'verifiedRuns': verifiedRuns,
-                    'rejectedRuns': rejectedRuns}
+    allRunsByGame = getRunsByGame(data)
+    runCountData = {
+        "totalRuns": len(data),
+        "verifiedRuns": verifiedRuns,
+        "rejectedRuns": rejectedRuns,
+        "allRunsByGame": allRunsByGame,
+    }
 
     return runCountData
+
+
+def getRunsByGame(data):
+    freq = {}
+
+    # get frequency of each game (gameId: frequency)
+    for run in range(len(data)):
+        if data[run]["status"]["status"] == "verified":
+            if data[run]["game"] in freq:
+                freq[data[run]["game"]] += 1
+            else:
+                freq[data[run]["game"]] = 1
+
+    newFreq = {}
+    # get gameName of each game (gameName: frequency)
+    for key, value in sorted(freq.items(), key=lambda x: x[1], reverse=True):
+        gameName = getGameName(key)
+        newFreq[gameName] = value
+
+    return newFreq
+
+
+def getGameName(gameId):
+    url = "https://www.speedrun.com/api/v1/games/{}".format(gameId)
+    data = getDataFromJson(url)
+
+    if not data:
+        return {}
+
+    return data["names"]["international"]
 
 
 # getUserProfile('LRF_Series')
 # getPersonalBests('7j4zz75x')
 
 # getDataFromJson('https://www.speedrun.com/api/v1/users?lookup=tyshoe')
-# getUserProfile('tyshoe')
-# getPersonalBests('jmo3vke8')
+# getUserProfile("tyshoe")
+# getPersonalBests("jmo3vke8")
+getRunCount("jmo3vke8")
 
 # getRunCount('jmo3vke8')
--- src/userinterface.py
+++ src/userinterface.py
@@ -23,30 +23,36 @@ class App(customtkinter.CTk):
         self.resizable(False, False)
 
         # Set Images
-        twitchLogo = ImageTk.PhotoImage(Image.open(
-            'resources/twitchLogo.png').resize((20, 20)))
-        youtubeLogo = ImageTk.PhotoImage(Image.open(
-            'resources/youtubeLogo.png').resize((20, 20)))
-        speedrunsLiveLogo = ImageTk.PhotoImage(Image.open(
-            'resources/speedrunsLiveLogo.png').resize((20, 20)))
-        twitterLogo = ImageTk.PhotoImage(Image.open(
-            'resources/twitterLogo.png').resize((20, 20)))
+        twitchLogo = ImageTk.PhotoImage(
+            Image.open("resources/twitchLogo.png").resize((20, 20))
+        )
+        youtubeLogo = ImageTk.PhotoImage(
+            Image.open("resources/youtubeLogo.png").resize((20, 20))
+        )
+        speedrunsLiveLogo = ImageTk.PhotoImage(
+            Image.open("resources/speedrunsLiveLogo.png").resize((20, 20))
+        )
+        twitterLogo = ImageTk.PhotoImage(
+            Image.open("resources/twitterLogo.png").resize((20, 20))
+        )
         firstPlace = ImageTk.PhotoImage(
-            Image.open('resources/1st.png').resize((20, 20)))
+            Image.open("resources/1st.png").resize((20, 20))
+        )
         secondPlace = ImageTk.PhotoImage(
-            Image.open('resources/2nd.png').resize((20, 20)))
+            Image.open("resources/2nd.png").resize((20, 20))
+        )
         thirdPlace = ImageTk.PhotoImage(
-            Image.open('resources/3rd.png').resize((20, 20)))
+            Image.open("resources/3rd.png").resize((20, 20))
+        )
 
         # Configure size of top and main frame
         self.columnconfigure(0, weight=1)
-        self.rowconfigure(0, weight=0, minsize=HEIGHT*.15)
-        self.rowconfigure(1, weight=1, minsize=HEIGHT*.85)
+        self.rowconfigure(0, weight=0, minsize=HEIGHT * 0.15)
+        self.rowconfigure(1, weight=1, minsize=HEIGHT * 0.85)
 
         # Create top frame and configure grid size
-        frameTop = customtkinter.CTkFrame(
-            self, fg_color='#3B3F44', corner_radius=0)
-        frameTop.grid(row=0, column=0, sticky='NSWE')
+        frameTop = customtkinter.CTkFrame(self, fg_color="#3B3F44", corner_radius=0)
+        frameTop.grid(row=0, column=0, sticky="NSWE")
         # TODO: create grid within top frame to align components
 
         def searchUser():
@@ -64,115 +70,197 @@ class App(customtkinter.CTk):
                 # Get user data
                 userProfile = datarequest.getUserProfile(userName)
                 personalBestsData = datarequest.getPersonalBests(
-                    userProfile.get('userId'))
-                runCountData = datarequest.getRunCount(
-                    userProfile.get('userId'))
+                    userProfile.get("userId")
+                )
+                runCountData = datarequest.getRunCount(userProfile.get("userId"))
 
                 profileFrame = customtkinter.CTkFrame(frameMain)
                 profileFrame.grid(row=0, column=0, padx=10, pady=10)
 
                 # Display user data
-                self.userNameLabel = customtkinter.CTkLabel(profileFrame, text="{}".format(userProfile.get(
-                    'userName')), font=customtkinter.CTkFont(size=20, weight="bold"), fg_color='red', width=150)
-                self.userNameLabel.grid(
-                    row=0, column=0, padx=(5, 10), pady=(5, 5))
+                self.userNameLabel = customtkinter.CTkLabel(
+                    profileFrame,
+                    text="{}".format(userProfile.get("userName")),
+                    font=customtkinter.CTkFont(size=20, weight="bold"),
+                    fg_color="red",
+                    width=150,
+                )
+                self.userNameLabel.grid(row=0, column=0, padx=(5, 10), pady=(5, 5))
 
                 self.speedrunLink = customtkinter.CTkLabel(
-                    profileFrame, text='', image=firstPlace, compound='right', fg_color='blue')
+                    profileFrame,
+                    text="",
+                    image=firstPlace,
+                    compound="right",
+                    fg_color="blue",
+                )
                 self.speedrunLink.bind(
-                    "<Button-1>", lambda e: self.callback(userProfile.get('speedrunLink')))  # click to open link
+                    "<Button-1>",
+                    lambda e: self.callback(userProfile.get("speedrunLink")),
+                )  # click to open link
                 self.speedrunLink.grid(row=0, column=1, pady=(5, 5))
 
-                if userProfile.get('youtubeLink') is None:
+                if userProfile.get("youtubeLink") is None:
                     self.youtubeLink = customtkinter.CTkLabel(
-                        profileFrame, text='', image=youtubeLogo, compound='right', fg_color='green')
-                    self.youtubeLink.bind("<Button>", lambda e: self.callback(
-                        userProfile.get('youtubeLink')))  # click to open link
+                        profileFrame,
+                        text="",
+                        image=youtubeLogo,
+                        compound="right",
+                        fg_color="green",
+                    )
+                    self.youtubeLink.bind(
+                        "<Button>",
+                        lambda e: self.callback(userProfile.get("youtubeLink")),
+                    )  # click to open link
                     self.youtubeLink.grid(row=0, column=2, pady=(5, 5))
 
-                if userProfile.get('twitchLink') is None:
+                if userProfile.get("twitchLink") is None:
                     self.twitchLink = customtkinter.CTkLabel(
-                        profileFrame, text='', image=twitchLogo, compound='right', fg_color='purple')
-                    self.twitchLink.bind("<Button>", lambda e: self.callback(
-                        userProfile.get('twitchLink')))  # click to open link
+                        profileFrame,
+                        text="",
+                        image=twitchLogo,
+                        compound="right",
+                        fg_color="purple",
+                    )
+                    self.twitchLink.bind(
+                        "<Button>",
+                        lambda e: self.callback(userProfile.get("twitchLink")),
+                    )  # click to open link
                     self.twitchLink.grid(row=0, column=3, pady=(5, 5))
 
-                if userProfile.get('speedrunsLiveLink') is None:
+                if userProfile.get("speedrunsLiveLink") is None:
                     self.speedrunsLiveLink = customtkinter.CTkLabel(
-                        profileFrame, text='', image=speedrunsLiveLogo, compound='right', fg_color='yellow')
-                    self.speedrunsLiveLink.bind("<Button>", lambda e: self.callback(
-                        userProfile.get('speedrunsLiveLink')))  # click to open link
+                        profileFrame,
+                        text="",
+                        image=speedrunsLiveLogo,
+                        compound="right",
+                        fg_color="yellow",
+                    )
+                    self.speedrunsLiveLink.bind(
+                        "<Button>",
+                        lambda e: self.callback(userProfile.get("speedrunsLiveLink")),
+                    )  # click to open link
                     self.speedrunsLiveLink.grid(row=0, column=4, pady=(5, 5))
 
-                if userProfile.get('twitterLink') is None:
+                if userProfile.get("twitterLink") is None:
                     self.twitterLink = customtkinter.CTkLabel(
-                        profileFrame, text='', image=twitterLogo, compound='right', fg_color='white')
-                    self.twitterLink.bind("<Button>", lambda e: self.callback(
-                        userProfile.get('twitterLink')))  # click to open link
+                        profileFrame,
+                        text="",
+                        image=twitterLogo,
+                        compound="right",
+                        fg_color="white",
+                    )
+                    self.twitterLink.bind(
+                        "<Button>",
+                        lambda e: self.callback(userProfile.get("twitterLink")),
+                    )  # click to open link
                     self.twitterLink.grid(row=0, column=5, pady=(5, 5))
 
-                self.signUpDateLabel = customtkinter.CTkLabel(profileFrame, text="Member Since: {}".format(
-                    userProfile.get('userSignup')), font=customtkinter.CTkFont(size=20))
+                self.signUpDateLabel = customtkinter.CTkLabel(
+                    profileFrame,
+                    text="Member Since: {}".format(userProfile.get("userSignup")),
+                    font=customtkinter.CTkFont(size=20),
+                )
                 self.signUpDateLabel.grid(
-                    row=2, column=0, padx=10, pady=5, columnspan=6)
+                    row=2, column=0, padx=10, pady=5, columnspan=6
+                )
 
                 ###
-                self.totalRunsLabel = customtkinter.CTkLabel(frameMain, text="Total Runs: {}".format(
-                    runCountData.get('totalRuns')), font=customtkinter.CTkFont(size=20))
+                self.totalRunsLabel = customtkinter.CTkLabel(
+                    frameMain,
+                    text="Total Runs: {}".format(runCountData.get("totalRuns")),
+                    font=customtkinter.CTkFont(size=20),
+                )
                 self.totalRunsLabel.grid(row=3, column=0, padx=10, pady=5)
 
-                self.verifiedRunsLabel = customtkinter.CTkLabel(frameMain, text="Verified Runs: {}".format(
-                    runCountData.get('verifiedRuns')), font=customtkinter.CTkFont(size=20))
+                self.verifiedRunsLabel = customtkinter.CTkLabel(
+                    frameMain,
+                    text="Verified Runs: {}".format(runCountData.get("verifiedRuns")),
+                    font=customtkinter.CTkFont(size=20),
+                )
                 self.verifiedRunsLabel.grid(row=4, column=0, padx=10, pady=5)
 
-                self.rejectedRunsLabel = customtkinter.CTkLabel(frameMain, text="Rejected Runs: {}".format(
-                    runCountData.get('rejectedRuns')), font=customtkinter.CTkFont(size=20))
+                self.rejectedRunsLabel = customtkinter.CTkLabel(
+                    frameMain,
+                    text="Rejected Runs: {}".format(runCountData.get("rejectedRuns")),
+                    font=customtkinter.CTkFont(size=20),
+                )
                 self.rejectedRunsLabel.grid(row=5, column=0, padx=10, pady=5)
 
                 ###
-                self.personalBestsLabel = customtkinter.CTkLabel(frameMain, text="Personal Bests: {}".format(
-                    personalBestsData.get('personalBests')), font=customtkinter.CTkFont(size=20))
+                self.personalBestsLabel = customtkinter.CTkLabel(
+                    frameMain,
+                    text="Personal Bests: {}".format(
+                        personalBestsData.get("personalBests")
+                    ),
+                    font=customtkinter.CTkFont(size=20),
+                )
                 self.personalBestsLabel.grid(row=6, column=0, padx=10, pady=5)
 
                 ###
                 placementsFrame = customtkinter.CTkFrame(frameMain)
                 placementsFrame.grid(row=7, column=0)
-                self.title = customtkinter.CTkLabel(placementsFrame, text="Podium", font=customtkinter.CTkFont(
-                    size=20, weight="bold"))
+                self.title = customtkinter.CTkLabel(
+                    placementsFrame,
+                    text="Podium",
+                    font=customtkinter.CTkFont(size=20, weight="bold"),
+                )
                 self.title.grid(row=0, column=0, padx=(5, 10), pady=(5, 5))
-                self.firstPlaceLabel = customtkinter.CTkLabel(placementsFrame, text=" {}".format(personalBestsData.get(
-                    'firstPlace')), font=customtkinter.CTkFont(size=20, weight="bold"), image=firstPlace, compound='left')
+                self.firstPlaceLabel = customtkinter.CTkLabel(
+                    placementsFrame,
+                    text=" {}".format(personalBestsData.get("firstPlace")),
+                    font=customtkinter.CTkFont(size=20, weight="bold"),
+                    image=firstPlace,
+                    compound="left",
+                )
                 self.firstPlaceLabel.grid(row=1, column=0, padx=10)
-                self.secondPlaceLabel = customtkinter.CTkLabel(placementsFrame, text=" {}".format(personalBestsData.get(
-                    'secondPlace')), font=customtkinter.CTkFont(size=20, weight="bold"), image=secondPlace, compound='left')
+                self.secondPlaceLabel = customtkinter.CTkLabel(
+                    placementsFrame,
+                    text=" {}".format(personalBestsData.get("secondPlace")),
+                    font=customtkinter.CTkFont(size=20, weight="bold"),
+                    image=secondPlace,
+                    compound="left",
+                )
                 self.secondPlaceLabel.grid(row=2, column=0, padx=10)
-                self.thirdPlaceLabel = customtkinter.CTkLabel(placementsFrame, text=" {}".format(personalBestsData.get(
-                    'thirdPlace')), font=customtkinter.CTkFont(size=20, weight="bold"), image=thirdPlace, compound='left')
+                self.thirdPlaceLabel = customtkinter.CTkLabel(
+                    placementsFrame,
+                    text=" {}".format(personalBestsData.get("thirdPlace")),
+                    font=customtkinter.CTkFont(size=20, weight="bold"),
+                    image=thirdPlace,
+                    compound="left",
+                )
                 self.thirdPlaceLabel.grid(row=3, column=0, padx=10)
             except IndexError:
                 self.errorLabel = customtkinter.CTkLabel(
-                    frameMain, text="Please enter a valid user name", font=customtkinter.CTkFont(size=20, weight="bold"))
+                    frameMain,
+                    text="Please enter a valid user name",
+                    font=customtkinter.CTkFont(size=20, weight="bold"),
+                )
                 self.errorLabel.grid(row=6, column=0, padx=10, pady=10)
 
         # Top frame
         self.entry = customtkinter.CTkEntry(
-            frameTop, placeholder_text="Username", width=WIDTH*.20)
-        self.entry.grid(row=0, column=0, padx=(
-            20, 5), pady=(25, 25), sticky="n")
+            frameTop, placeholder_text="Username", width=WIDTH * 0.20
+        )
+        self.entry.grid(row=0, column=0, padx=(20, 5), pady=(25, 25), sticky="n")
         self.search_button = customtkinter.CTkButton(
-            frameTop, width=40, text='Search', command=searchUser)
+            frameTop, width=40, text="Search", command=searchUser
+        )
         self.search_button.grid(row=0, column=1, padx=(5, 10))
         self.button = customtkinter.CTkButton(
-            frameTop, width=40, text='speedrun.com REST API', command=self.openRestApi)
+            frameTop, width=40, text="speedrun.com REST API", command=self.openRestApi
+        )
         self.button.grid(row=0, column=2, padx=10, pady=10)
         self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(
-            frameTop, values=["System", "Dark", "Light"], command=self.change_appearance_mode_event)
-        self.appearance_mode_optionemenu.grid(
-            row=0, column=3, padx=10, pady=10)
+            frameTop,
+            values=["System", "Dark", "Light"],
+            command=self.change_appearance_mode_event,
+        )
+        self.appearance_mode_optionemenu.grid(row=0, column=3, padx=10, pady=10)
 
         # Create main frame and configure grid size
         frameMain = customtkinter.CTkFrame(self, corner_radius=0)
-        frameMain.grid(row=1, column=0, sticky='NSWE')
+        frameMain.grid(row=1, column=0, sticky="NSWE")
 
     def change_appearance_mode_event(self, new_appearance_mode: str):
         customtkinter.set_appearance_mode(new_appearance_mode)
@@ -182,4 +270,5 @@ class App(customtkinter.CTk):
 
     def openRestApi(self):
         webbrowser.open_new_tab(
-            'https://github.com/speedruncomorg/api#speedruncom-rest-api')
+            "https://github.com/speedruncomorg/api#speedruncom-rest-api"
+        )
